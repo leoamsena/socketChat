860ff779c9bda180115e7374efb7d43de541f120
Leonardo Amorim <leonardo123.amorim@gmail.com>
2020-08-25 18:00:22 -0300

    Alterações interface

--- src/client/ui/Login.java
+++ src/client/ui/Login.java
@@ -12,40 +12,45 @@ public class Login extends JPanel {
 
     public Login() {
 
-        this.setLayout(new BorderLayout());
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
         JLabel jl = new JLabel("CHAT!", SwingConstants.CENTER);
         jl.setFont(new Font("Serif", Font.PLAIN, 50));
-        this.add(BorderLayout.NORTH, jl);
+        this.add(jl);
 
+        jl = new JLabel("Explicando os bagulhos doido do chat!");
+        this.add(jl);
+        /*
+        JTextField teste = new JTextField();
+        teste.setColumns(20);
+        this.add(teste);
+        */
         this.host = new JTextField();
         this.host.setColumns(50);
         this.nome = new JTextField();
-        this.nome.setColumns(56);
+        this.nome.setColumns(55);
         this.porta = new JTextField();
         this.porta.setColumns(4);
 
-        JPanel jpCenter = new JPanel();
-
-        jpCenter.setLayout(new BoxLayout(jpCenter, BoxLayout.Y_AXIS));
-
         JPanel firstLine = new JPanel();
+
         JPanel secondLine = new JPanel();
 
         firstLine.add(this.host);
         firstLine.add(this.porta);
 
+        firstLine.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        this.add(firstLine);
+
         secondLine.add(this.nome);
-
-        jpCenter.add(firstLine);
-
-        jpCenter.add(secondLine);
-
-        this.add(BorderLayout.CENTER, jpCenter);
+        secondLine.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+        this.add(secondLine);
 
         JButton jb = new JButton("Entrar!");
         jb.setFont(new Font("Serif", Font.PLAIN, 50));
         jb.addActionListener(e -> ChatClient.logar(this.host.getText(), this.porta.getText(), this.nome.getText()));
-        this.add(BorderLayout.SOUTH, jb);
+        this.add(jb);
 
     }
 }
